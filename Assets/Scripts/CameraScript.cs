0245e93d64fc746184ae030a36f2bbe24536b27e
Ekaterina <ekaterina.krysenkova@student.htw-berlin.de>
2021-05-31 23:16:55 +0200

    LevelOne start

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 0.01f;
+    private float distance = 1f;
+    private Vector3 offset;
+    public PathCreator pathCreator;
+
+    void Start()
+    {
+        //let camera be behind the player while going along the path
+      offset  = new Vector3(0f, 0.3f, 0f);
+    }
+
+    void Update()
+    {
+        //camera follows th path
+        distance += speed * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(distance-1.5f)+ offset; 
+        transform.rotation = pathCreator.path.GetRotationAtDistance(distance-1.5f);
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public PathCreator pathCreator;
+    public float speed = 0.01f;
+    private float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //player follows the path
+        distance += speed * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(distance);
+        transform.rotation = pathCreator.path.GetRotationAtDistance(distance);
+    }
+}
